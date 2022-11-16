12ab10d67b840d00e9cff78bcc2e87702f43e7e0
Donald Johnson <donaldrjohnson66@gmail.com>
2022-11-15 21:17:49 -0600

    Finished up!

---
+++ Variables-Exercise/Program.cs
@@ -0,0 +1,11 @@
+﻿string girlfriendName = "Merritt";
+bool veryLovely = true;
+int girlfriendAge = 23;
+decimal relationshipLength = 1.1m;
+char sibling = '1';
+double girlfriendHeight = 5.4;
+Console.WriteLine($"My girlfriend's name is {girlfriendName}. She is {girlfriendAge} and {girlfriendHeight} feet tall! I'm lucky to have been together with her for {relationshipLength} years.");
+Console.WriteLine($" Is she very pretty? that would so happen to be {veryLovely}. She also happens to have {sibling} sibling who is a identical twin which is super neat.");
+
+
+
